6a2d5a66d7ed48f2221d41902b07886f5e10c41f
therealjuancai <86586208+therealjuancai@users.noreply.github.com>
2024-04-11 15:43:58 -0500

    ca

---
+++ app/src/main/java/com/softcg/myapplication/ui/register/model/UserRegister.kt
@@ -0,0 +1,11 @@
+package com.softcg.myapplication.ui.register.model
+
+data class UserRegister (
+    val email: String,
+    val password: String,
+    val passwordConfirmation: String
+)
+{
+    fun isNotEmpty()=
+        email.isNotEmpty()&&password.isNotEmpty()&&passwordConfirmation.isNotEmpty()
+}
