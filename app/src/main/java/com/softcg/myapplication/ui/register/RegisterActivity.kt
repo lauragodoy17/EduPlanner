70d448363d23802fee16b8db722b109ebddd6a8a
therealjuancai <86586208+therealjuancai@users.noreply.github.com>
2024-04-11 22:48:40 -0500

    avances

--- app/src/main/java/com/softcg/myapplication/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/softcg/myapplication/ui/register/RegisterActivity.kt
@@ -7,14 +7,20 @@ import android.view.inputmethod.EditorInfo
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.utils.ViewState
+import androidx.core.view.isVisible
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.softcg.myapplication.R
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.softcg.myapplication.core.dialog.DialogFragmentLauncher
-import com.softcg.myapplication.core.ex.loseFocusAfterAction
+import com.softcg.myapplication.core.dialog.ErrorDialog
+import com.softcg.myapplication.core.ex.*
 import com.softcg.myapplication.databinding.ActivityRegisterBinding
+import com.softcg.myapplication.ui.home.HomeActivity
 import com.softcg.myapplication.ui.login.MainActivity
+import com.softcg.myapplication.ui.register.model.UserRegister
 
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -53,9 +59,24 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun initlisteners(){
         binding.campoemail.loseFocusAfterAction(EditorInfo.IME_ACTION_NEXT)
+        binding.campoemail.setOnFocusChangeListener{_, hasFocus -> onFieldChanged(hasFocus)}
+        binding.campoemail.onTextChanged { onFieldChanged() }
+
+        binding.campocontrasena.loseFocusAfterAction(EditorInfo.IME_ACTION_NEXT)
+        binding.campocontrasena.setOnFocusChangeListener{_, hasFocus -> onFieldChanged(hasFocus)}
+        binding.campocontrasena.onTextChanged { onFieldChanged() }
+
+        binding.campoconfirmarcontra.loseFocusAfterAction(EditorInfo.IME_ACTION_NEXT)
+        binding.campoconfirmarcontra.setOnFocusChangeListener{_, hasFocus -> onFieldChanged(hasFocus)}
+        binding.campoconfirmarcontra.onTextChanged { onFieldChanged() }
+
         with(binding){
             tvIniciaSesion.setOnClickListener{viewmodelregister.onLoginSelected()}
             Boton1.setOnClickListener{viewmodelregister.onLoginSelected()}
+            botonregistro.setOnClickListener {
+                it.dismissKeyboard()
+                //viewmodelregister.onRegisterSelected()
+            }
         }
     }
 
@@ -66,8 +87,31 @@ class RegisterActivity : AppCompatActivity() {
             }
         })
     }
+
+    private fun showErrorDialog() {
+        ErrorDialog.create(
+            title = getString(R.string.signin_error_dialog_title),
+            description = getString(R.string.signin_error_dialog_body),
+            positiveAction = ErrorDialog.Action(getString(R.string.signin_error_dialog_positive_action)) {
+                it.dismiss()
+            }
+        ).show(dialogLauncher, this)
+    }
+
+    //private fun updateUI(viewState: )
+
     private fun goToRegister(){
         startActivity(MainActivity.create(this))
+    }
+
+    private fun onFieldChanged(hasFocus: Boolean=false){
+        if (!hasFocus){
+            //viewmodelregister.
+        }
+    }
+
+    private fun goToHome(){
+        //startActivity(HomeActivity.create(this))
     }
 
 }
