02ece6d4cb9e6fc6fec147b3a44af1434d5e8ed6
therealjuancai <86586208+therealjuancai@users.noreply.github.com>
2024-04-15 18:46:09 -0500

    menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,11 +67,13 @@ dependencies {
     //ojito
     implementation ("com.google.android.material:material:1.5.0")
 
-
     //Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
+
+    //Menú
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/softcg/myapplication/ui/home/HomeActivity.kt
+++ app/src/main/java/com/softcg/myapplication/ui/home/HomeActivity.kt
@@ -3,15 +3,21 @@ package com.softcg.myapplication.ui.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.softcg.myapplication.R
 import com.softcg.myapplication.ui.register.RegisterActivity
 
 class HomeActivity : AppCompatActivity() {
 
+    lateinit var toggle: ActionBarDrawerToggle
     companion object{
         fun create(context: Context): Intent =
             Intent(context, HomeActivity::class.java)
@@ -20,10 +26,32 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val  drawerLayout: DrawerLayout = findViewById(R.id.main)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext,"a",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
