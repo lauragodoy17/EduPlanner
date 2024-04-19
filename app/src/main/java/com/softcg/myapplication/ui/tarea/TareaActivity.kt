0a13e7266f0de7daa6f57cf8946497b51518f465
Mondapodrida <godoylaura332@gmail.com>
2024-04-18 22:55:52 -0500

    calendario

--- app/src/main/java/com/softcg/myapplication/ui/tarea/TareaActivity.kt
+++ app/src/main/java/com/softcg/myapplication/ui/tarea/TareaActivity.kt
@@ -1,8 +1,11 @@
 package com.softcg.myapplication.ui.tarea
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.softcg.myapplication.R
 import com.softcg.myapplication.ui.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 
 @AndroidEntryPoint
 class TareaActivity : AppCompatActivity() {
@@ -27,5 +31,15 @@ class TareaActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun onClickScheduledDate(v: View?){
+        val etScheduledDate= findViewById<EditText>(R.id.FechaEditText)
+        val selectedCalendar= Calendar.getInstance()
+        val year=selectedCalendar.get(Calendar.YEAR)
+        val month= selectedCalendar.get(Calendar.MONTH)
+        val dayOfMonth=selectedCalendar.get(Calendar.DAY_OF_MONTH)
+        val listener= DatePickerDialog.OnDateSetListener{datePicker,y,m,d-> etScheduledDate.setText("$y-$m-$d")}
+        DatePickerDialog(this, R.style.CustomDatePickerDialogTheme,listener,year, month, dayOfMonth).show()
+
     }
 }
